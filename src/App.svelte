02bed8f7d5b4ff3ce9a3b7e4a85ab29b88e4c4ac
Mwungere <mwungereelite56@gmail.com>
2024-06-09 18:39:30 +0200

    header and footer

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Footer from "./components/Footer.svelte";
+import Header from "./components/Header.svelte";
+
+</script>
+
+<Header />
+
+<main>
+	<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet aspernatur amet, fugit voluptatibus temporibus magni deleniti sapiente reiciendis? Dicta reprehenderit deleniti amet ipsa adipisci facilis libero quae ad veritatis optio.</p>
+</main>
+
+<Footer/>
+
+<style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
